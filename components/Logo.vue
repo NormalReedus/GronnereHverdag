c8fbf7ca716646e6f7f7b95ff7388a1c09c7e39f
Magnus <magnus.borregaard@gmail.com>
2020-10-17 13:22:12 +0200

    add ts & class comp

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hey from logo</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class NcGridSection extends Vue {}
+</script>
+<style scoped>
+.section {
+  /* dev */
+  border: 1px dotted red;
+}
+</style>
