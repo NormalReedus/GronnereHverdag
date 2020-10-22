1b26b12677db127c260f3b16a569ab073efb48c9
Magnus <magnus.borregaard@gmail.com>
2020-10-22 10:21:46 +0200

    rm content call

--- components/ArticleCardContainer.vue
+++ components/ArticleCardContainer.vue
@@ -11,25 +11,11 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { IArticleCard } from '@/types'
+import { Context } from '@nuxt/types'
 
 @Component
 export default class ArticleCard extends Vue {
   // @Prop({ required: true }) cards!: IArticleCard[]
-
-  async asyncData({
-    $content,
-  }: {
-    store: any
-    $content: any
-  }): Promise<{ articleCards: IArticleCard[] }> {
-    const articleCards = await $content('articles')
-      .only(['date', 'image', 'path', 'summary', 'tags', 'title'])
-      .fetch()
-
-    return {
-      articleCards,
-    }
-  }
 }
 </script> 
 
